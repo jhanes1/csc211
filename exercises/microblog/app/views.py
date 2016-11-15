57329f2ae2fb1e1786ab0578c9c0ab90c8a13d4b
jhanes1 <jdhanes1@gmail.com>
2016-11-15 07:55:53 -0600

    webforms page complete

--- exercises/microblog/app/views.py
+++ exercises/microblog/app/views.py
@@ -25,4 +25,7 @@ def login():
         flash('Login requested for OpenID="{}", remember_me={}'.\
             format(form.openid.data, str(form.remember_me.data)))
         return redirect('/idex')
-    return render_template('login.html',title='Sign In',form=form)
+    return render_template('login.html',
+        title='Sign In',
+        form=form,
+        providers=app.config['OPENID_PROVIDERS'])
